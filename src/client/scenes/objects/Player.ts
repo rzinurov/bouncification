999f4f10cce79fb70f9d297d304527f9e0764934
Rustam Zinurov <rustamz@spotify.com>
2022-01-01 12:48:54 +0100

    Refactor Player class

--- src/client/scenes/objects/Player.ts
+++ src/client/scenes/objects/Player.ts
@@ -3,30 +3,21 @@ import { PlayerState } from "common/schema/PlayerState";
 import WorldConfig from "common/consts/WorldConfig";
 import PlayerPhysics from "common/physics/PlayerPhysics";
 
-export default class Player extends Phaser.GameObjects.Container {
-  matterBody: Phaser.Physics.Matter.Sprite;
+export default class Player extends Phaser.Physics.Matter.Image {
   nameLabel: Phaser.GameObjects.Text;
 
   constructor(scene: Phaser.Scene, state: PlayerState, imageName: string) {
-    super(scene, state.position.x, state.position.y);
-    this.width = WorldConfig.player.spriteSize;
-    this.height = WorldConfig.player.spriteSize;
+    super(scene.matter.world, state.position.x, state.position.y, imageName);
 
-    const image = scene.add.image(0, 0, imageName);
-    this.add(image);
+    this.setCircle(WorldConfig.player.spriteSize / 2);
+    this.setBounce(WorldConfig.player.restitution);
 
     this.nameLabel = this.scene.add
-      .text(0, -this.height * 0.9, state.name, {
+      .text(0, 0, state.name, {
         font: `32px Arial`,
         color: "#ffdd00",
       })
       .setOrigin(0.5) as Phaser.GameObjects.Text;
-
-    this.matterBody = scene.matter.add.gameObject(
-      this
-    ) as Phaser.Physics.Matter.Sprite;
-    this.matterBody.setCircle(this.width / 2);
-    this.matterBody.setBounce(WorldConfig.player.restitution);
 
     this.scene.add.existing(this);
 
@@ -34,32 +25,39 @@ export default class Player extends Phaser.GameObjects.Container {
   }
 
   jumpTo(pointer: { x: number; y: number }) {
-    const velocity = PlayerPhysics.getVelocity(this.matterBody, pointer);
-    this.matterBody.setVelocity(velocity.x, velocity.y);
+    const velocity = PlayerPhysics.getVelocity(this, pointer);
+    this.setVelocity(velocity.x, velocity.y);
   }
 
   updateState(state: PlayerState) {
     if (this.checkVectorDiff(this, state.position)) {
-      this.matterBody.setPosition(state.position.x, state.position.y);
-      this.matterBody.setAwake();
+      this.setPosition(state.position.x, state.position.y);
+      this.setAwake();
     }
     if (this.checkNumberDiff(this.angle, state.angle)) {
-      this.matterBody.setAngle(state.angle);
-      this.matterBody.setAwake();
+      this.setAngle(state.angle);
+      this.setAwake();
     }
-    this.matterBody.setVelocity(state.velocity.x, state.velocity.y);
-    this.matterBody.setAngularVelocity(state.angularVelocity);
+    this.setVelocity(state.velocity.x, state.velocity.y);
+    this.setAngularVelocity(state.angularVelocity);
   }
 
-  checkVectorDiff(a: { x: number; y: number }, b: { x: number; y: number }) {
+  private checkVectorDiff(
+    a: { x: number; y: number },
+    b: { x: number; y: number }
+  ) {
     return this.checkNumberDiff(a.x, b.x) || this.checkNumberDiff(a.y, b.y);
   }
 
-  checkNumberDiff(a: number, b: number) {
+  private checkNumberDiff(a: number, b: number) {
     return Math.abs(a - b) >= 0.5;
   }
 
   preUpdate() {
+    this.updateNameLabelPosition();
+  }
+
+  private updateNameLabelPosition() {
     this.nameLabel.setPosition(this.x, this.y - this.height * 0.75);
   }
 
